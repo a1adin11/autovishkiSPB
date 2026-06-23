5d9bf1753aa62ad22f7a271a31903082f715fac9
a1adin11 <alexsad2004@gmail.com>
2026-06-23 23:42:34 +0300

    Fix iOS Safari safe area in header

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,6 +70,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+      </head>
       <body className={inter.className}>
         <Header />
         {children}
